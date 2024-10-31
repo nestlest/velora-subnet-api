e7a321d12842e1b607b2deb40cd381a935c87744
drunest <venus1004@proton.me>
2024-10-31 13:13:18 -0500

    feat: add setting config for etherem rcp node url

--- velora/settings.py
+++ velora/settings.py
@@ -131,3 +131,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+ETHEREUM_RPC_NODE_URL=env("ETHEREUM_RPC_NODE_URL")
